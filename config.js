54c7c1198dba5a2951148aa627a96792d82b82bc
jobysanthosh <joby.santhosh@smoothstack.com>
2019-12-08 16:53:13 -0500

    env

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
-// require('dotenv').config();//instatiate environment variables
+require('dotenv').config();//instatiate environment variables
 
-// let CONFIG = {} //Make this global to use all over the application
+let CONFIG = {} //Make this global to use all over the application
 
-// CONFIG.db_dialect   = process.env.DB_DIALECT
-// CONFIG.db_host      = process.env.DB_HOST
-// CONFIG.db_port      = process.env.DB_PORT
-// CONFIG.db_name      = process.env.DB_NAME
-// CONFIG.db_user      = process.env.DB_USER
-// CONFIG.db_password  = process.env.DB_PASSWORD
-// CONFIG.stripe_secretkey = process.env.STRIPE_SECRETKEY
-// CONFIG.digestAlgorithm = process.env.DIGESTALGORITHM
+CONFIG.db_dialect   = process.env.DB_DIALECT
+CONFIG.db_host      = process.env.DB_HOST
+CONFIG.db_port      = process.env.DB_PORT
+CONFIG.db_name      = process.env.DB_NAME
+CONFIG.db_user      = process.env.DB_USER
+CONFIG.db_password  = process.env.DB_PASSWORD
+CONFIG.stripe_secretkey = process.env.STRIPE_SECRETKEY
+CONFIG.digestAlgorithm = process.env.DIGESTALGORITHM
 
-// CONFIG.testdb_host  = process.env.TESTDB_HOST
-// CONFIG.testdb_user  = process.env.TESTDB_USER
-// CONFIG.testdb_password  = process.env.TESTDB_PASSWORD
+CONFIG.testdb_host  = process.env.TESTDB_HOST
+CONFIG.testdb_user  = process.env.TESTDB_USER
+CONFIG.testdb_password  = process.env.TESTDB_PASSWORD
 
-// module.exports = CONFIG
+module.exports = CONFIG
--- dao/db.js
+++ dao/db.js
@@ -1,22 +1,21 @@
 var mysql      = require('mysql');
-// const CONFIG = require('../config');
+const CONFIG = require('../config');
 
 var connection = mysql.createConnection({
-    host     : db_host,
-    port     : '3306',
-    user     : 'admin',
-    password : 'teamflash123',
-    database : 'UtopiaAirline'
+    host     : CONFIG.db_host,
+    port     : CONFIG.db_port,
+    user     : CONFIG.db_user,
+    password : CONFIG.db_password,
+    database : CONFIG.db_name
 });
 
 connection.connect(function(err){
 if(!err) {
-    console.log("AWS Database is connected ... ");   
-    //console.log(connection.config.host); 
+    console.log("AWS Database is connected ... ");    
     
 } else {
     console.log("Error connecting AWS database ... ");   
-    //console.log(db_host); 
+    console.log(db_host); 
 
 }
 });
